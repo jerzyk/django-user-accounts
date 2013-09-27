b0d49b43a37aa44c944d29e573cff6ac3238e557
Janusz Harkot <jh@blueice.pl>
2013-09-27 21:27:56 +0200

    Django 1.5 Custom User Model

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.test.client import RequestFactory
 from django.utils import unittest
 
-from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.models import AnonymousUser
 
 from account.forms import SignupForm, LoginUsernameForm
 from account.views import SignupView, LoginView
